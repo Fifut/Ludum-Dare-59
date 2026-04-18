14c33fca6fca560983d9d5fbd36089f5a9dcd6a5
Fabien Streit <streit.fabien@gmail.com>
2026-04-18 10:45:41 +0200

    Sky and lights

--- Scenes/Receiver/receiver.gd
+++ Scenes/Receiver/receiver.gd
@@ -1,11 +1,16 @@
 extends AnimatableBody3D
 
 
+signal on_ld_signal_received()
+
+
 var _received_count: int = 0
 
 
 func _on_receiver_area_3d_body_entered(body: Node3D) -> void:
 	if body is LDSignal:
+		on_ld_signal_received.emit()
 		body.queue_free()
+		
 		_received_count += 1
 		print(_received_count)
